e4e86007b079b580d8421bafc3d76a32f9a50dc1
Vadim Tishenko <vad.tishenko@gmail.com>
2015-03-15 14:34:56 +0300

    ДЗ №3.  добавил meals в скрипты базы.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 -- password
@@ -11,3 +12,9 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES ('ROLE_USER', 100000);
 INSERT INTO user_roles (role, user_id) VALUES ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals(date_time, description, calories, user_id) VALUES ('20150310','meal1',100,100000);
+INSERT INTO meals(date_time, description, calories, user_id) VALUES ('20150311','meal2',200,100000);
+INSERT INTO meals(date_time, description, calories, user_id) VALUES ('20150312','meal3',350,100000);
+
+
--- src/test/java/ru/javawebinar/topjava/web/mock/UserAdminSpringMockTest.java
+++ src/test/java/ru/javawebinar/topjava/web/mock/UserAdminSpringMockTest.java
@@ -1,12 +1,15 @@
 package ru.javawebinar.topjava.web.mock;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.model.BaseEntity;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.util.DbPopulator;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 import ru.javawebinar.topjava.web.user.AdminUserRestController;
 
@@ -14,12 +17,23 @@ import ru.javawebinar.topjava.web.user.AdminUserRestController;
  * GKislin
  * 13.03.2015.
  */
-@ContextConfiguration("classpath:spring/spring-app.xml")
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserAdminSpringMockTest {
 
     @Autowired
     private AdminUserRestController controller;
+
+    @Autowired
+    private DbPopulator dbPopulator;
+
+    @Before
+    public void setUp() throws Exception {
+        dbPopulator.execute();
+    }
 
     @Test
     public void testCreate() throws Exception {
@@ -28,7 +42,7 @@ public class UserAdminSpringMockTest {
 
     @Test
     public void testDelete() throws Exception {
-        controller.delete(7);
+        controller.delete(BaseEntity.START_SEQ);
     }
 
     @Test(expected = NotFoundException.class)
